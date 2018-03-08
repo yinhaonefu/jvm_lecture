67f1c3f98982ad22a6d2d5f1d923e6becad1e3b2
yinhao-mac <yinhao@jd.com>
2018-03-08 22:09:47 +0800

    Initial commit

---
+++ src/main/java/com/shengsiyuan/jvm/classloader/MyTest1.java
@@ -0,0 +1,46 @@
+package com.shengsiyuan.jvm.classloader;
+
+/**
+ * Created by yinhao on 18/3/8.
+ *
+ * 对于静态变量来说，只有直接定义了该静态变量的类才会被初始化
+ * 当一个类在初始化时，要求其父类全部都已经初始化完毕
+ */
+public class MyTest1 {
+
+    public static void main(String[] args) {
+
+//        System.out.println(MyChild1.str);
+
+//        输出
+//        MyParent1 static block
+//        hello world
+
+        System.out.println("-----------");
+
+        System.out.println(MyChild1.str2);
+
+//        输出
+//        MyParent1 static block
+//        MyChild1 static block
+//        welcome
+    }
+}
+
+class MyParent1{
+
+    public static String str = "hello world";
+
+    static {
+        System.out.println("MyParent1 static block");
+    }
+}
+
+class MyChild1 extends MyParent1{
+
+    public static String str2 = "welcome";
+
+    static {
+        System.out.println("MyChild1 static block");
+    }
+}
