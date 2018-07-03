d03ea415674e5f3959ae5eefdba7e284a5d93325
yinhao <yinhao@jd.com>
2018-07-03 09:08:32 +0800

    11_类加载器双亲委托机制详解

--- src/main/java/com/shengsiyuan/jvm/classloader/MyTest7.java
+++ src/main/java/com/shengsiyuan/jvm/classloader/MyTest7.java
@@ -3,9 +3,9 @@ package com.shengsiyuan.jvm.classloader;
 public class MyTest7 {
     public static void main(String[] args)throws Exception {
         Class<?> sClass = Class.forName("java.lang.String");
-        System.out.println(sClass.getClassLoader());
+        System.out.println(sClass.getClassLoader());//由根类加载器加载 返回null 查看方法文档
 
         Class<?> mClass = Class.forName("com.shengsiyuan.jvm.classloader.MyTest7");
-        System.out.println(mClass.getClassLoader());
+        System.out.println(mClass.getClassLoader());//由系统加载器加载
     }
 }
