bbaeefe87be3e666a559e3c7a7bba7d97e9fd151
yinhao <yinhao@jd.com>
2020-04-01 09:21:01 +0800

    66_JVM元空间深度解析

--- src/main/java/com/shengsiyuan/jvm/memory/MyTest4.java
+++ src/main/java/com/shengsiyuan/jvm/memory/MyTest4.java
@@ -1,5 +1,11 @@
 package com.shengsiyuan.jvm.memory;
 
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
 /**
  * 方法区产生内存溢出错误
  * 从JDK1.8开始使用元空间（meta space）代替永久代，元空间就位于方法区中，使用的是操作系统的本地内存初试大小21m，由元空间虚拟机管理其内存大小
@@ -10,5 +16,25 @@ package com.shengsiyuan.jvm.memory;
  *
  */
 public class MyTest4 {
-
+    public static void main(String[] args) {
+        /**
+         * 使用cglib不断创建MyTest4的子类并限制元空间大小 -XX:MaxMetaspaceSize=5m
+         * 运行时通过jconsole观察加载的类的数量在不断增加 勾选详细输出控制台会打印详细的类加载日志，或者继续添加 -XX:+TraceClassLoading参数
+         * 
+         *
+         */
+        for ( ; ; ){
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(MyTest4.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor() {
+                @Override
+                public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                    return proxy.invokeSuper(obj, args);
+                }
+            });
+            System.out.println("hello world");
+            enhancer.create();
+        }
+    }
 }
