8c5c62c0ba8f06ca44ed5dca597ea2cad44306bf
yinhao <yinhao@jd.com>
2020-04-09 10:22:06 +0800

    57_透过字节码生成审视Java动态代理运作机制

---
+++ src/main/java/com/shengsiyuan/jvm/bytecode/Client.java
@@ -0,0 +1,17 @@
+package com.shengsiyuan.jvm.bytecode;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+public class Client {
+
+    public static void main(String[] args) {
+        //因为动态代理生成的代理类是运行期生成的，该属性标识着是否将生成的代理类保存在磁盘上 com.sun.proxy.$Proxy0.class
+        System.getProperties().setProperty("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+        RealSubject rs = new RealSubject();
+        InvocationHandler ds = new DynamicSubject(rs);
+        Class<?> cls = rs.getClass();
+        Subject subject = (Subject) Proxy.newProxyInstance(cls.getClassLoader(), cls.getInterfaces(), ds);
+        subject.request();
+    }
+}
